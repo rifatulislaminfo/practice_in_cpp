a4198e13865506e09d7e6bd228396887fe401910
Rifatul Islam <140544121+rifatulislaminfo@users.noreply.github.com>
2023-10-10 21:26:47 +0600

    Create laxicography.cpp

---
+++ laxicography.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+ 	string s="rifat";
+
+ 	string s1="abd";
+
+ 	if(s<s1){
+ 		cout<<"s is small"<<endl;
+ 	}
+ 	else{
+
+
+ 		cout<<"s2 is small"<<endl;
+ 	}
+	return 0;
+}
